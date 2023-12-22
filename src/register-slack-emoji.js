05bd4741a1912c8cc9fe49a835556d63661aec88
小野隆規 <tkkydrm3tn@yahoo.co.jp>
2023-12-23 04:00:25 +0900

    refact: 関数のコメントの修正 + 関数の切り出し方修正

--- src/register-slack-emoji.js
+++ src/register-slack-emoji.js
@@ -7,29 +7,21 @@ const { default: axios } = require("axios");
 // 環境変数の読み込み
 require("dotenv").config();
 const workspace = process.env.SLACK_WORKSPACE;
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
 
+/**
+ * LINEスタンプをSlack絵文字として登録する関数
+ * @param {string} emojiNamePrefix - 絵文字の登録名のプレフィックス
+ * @param {string} stampsFilePathList - LINEスタンプの画像ファイルのパスのリスト
+ * @returns {Promise<object[]>} - 絵文字の登録結果のリスト
+ */
 async function registerAllLineStampsToSlack(
   emojiNamePrefix,
   stampsFilePathList
 ) {
-  // Seleniumのドライバーをビルド
-  const driver = await buildSeleniumDriver();
-
-  let results = null;
-  let cookie = null;
-  let token = null;
-  try {
-    // サインイン
-    await signInToSlack(driver);
-
-    cookie = await getCookie(driver);
-    token = await getToken(driver);
-  } catch (error) {
-    results = error;
-  } finally {
-    // ドライバーを終了
-    await driver.quit();
-  }
+  // POSTリクエストに必要なCookieとAPIトークンを取得
+  const { cookie, token } = await getCookieAndToken();
 
   // 全ての絵文字の登録処理を並列で実行
   const registrationPromises = stampsFilePathList.map((stampsFilePath, idx) => {
@@ -42,13 +34,37 @@ async function registerAllLineStampsToSlack(
       token
     );
   });
+
   // すべての絵文字の登録処理が終わるまで待機
-  results = await Promise.all(registrationPromises);
+  const results = await Promise.all(registrationPromises);
   console.log(results);
   return results;
 }
 
-// Seleniumのドライバーをビルドする関数
+/**
+ * Seleniumを使ってサインインしてCookieとAPIトークンを取得する関数.
+ */
+async function getCookieAndToken() {
+  // Seleniumのドライバーをビルド
+  const driver = await buildSeleniumDriver();
+
+  try {
+    // サインイン
+    await signInToSlack(driver);
+
+    cookie = await getCookie(driver);
+    token = await getToken(driver);
+  } finally {
+    // ドライバーを終了
+    await driver.quit();
+  }
+  return { cookie, token };
+}
+
+/**
+ * Seleniumのドライバーをビルドする関数
+ * @returns {WebDriver} - Seleniumのドライバー
+ */
 async function buildSeleniumDriver() {
   // ヘッドレスモードのオプションを設定
   const options = new chrome.Options();
@@ -61,21 +77,22 @@ async function buildSeleniumDriver() {
     .build();
 }
 
-// Slackにサインインする関数
+/**
+ * Slackにサインインする関数
+ * @param {WebDriver} driver - Seleniumのドライバー
+ * @returns {Promise<void>} - Promise
+ */
 async function signInToSlack(driver) {
   console.log("Signing in to Slack...");
 
   // Slackにアクセス
-  const workspace = process.env.SLACK_WORKSPACE;
   await driver.get(
     `https://${workspace}.slack.com/sign_in_with_password?redir=%2Fcustomize%2Femoji#/`
   );
 
   // ログイン処理
-  await driver.findElement(By.id("email")).sendKeys(process.env.EMAIL);
-  await driver
-    .findElement(By.id("password"))
-    .sendKeys(process.env.PASSWORD, Key.RETURN);
+  await driver.findElement(By.id("email")).sendKeys(email);
+  await driver.findElement(By.id("password")).sendKeys(password, Key.RETURN);
 
   // 'boot_data'を含むscriptタグが読みこまれるのを待機
   await driver.wait(
@@ -89,18 +106,33 @@ async function signInToSlack(driver) {
   console.log("completed sign in to Slack");
 }
 
-// Cookieを取得する関数
+/**
+ * Cookieを取得する関数
+ * @param {WebDriver} driver - Seleniumのドライバー
+ * @returns {Promise<string>} - Cookie
+ */
 async function getCookie(driver) {
   const cookies = await driver.manage().getCookies();
   return cookies.map((cookie) => `${cookie.name}=${cookie.value}`).join("; ");
 }
 
-// APIトークンを取得する関数
+/**
+ * APIトークンを取得する関数
+ * @param {WebDriver} driver
+ * @returns {Promise<string>} - APIトークン
+ */
 async function getToken(driver) {
   return driver.executeScript("return window.boot_data.api_token;");
 }
 
-// 1つのLINEスタンプをSlack絵文字として登録する関数
+/**
+ * 1つのLINEスタンプをSlack絵文字として登録する関数
+ * @param {string} emojiName - 絵文字の登録名
+ * @param {string} stampsFilePath - LINEスタンプの画像ファイルのパス
+ * @param {string} cookie - Cookie
+ * @param {string} token - APIトークン
+ * @returns {Promise<object>} - 絵文字の登録結果
+ */
 async function registerOneLineStampToSlack(
   emojiName,
   stampsFilePath,
